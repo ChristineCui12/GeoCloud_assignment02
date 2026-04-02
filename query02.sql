7366cac17048db65e8a89746ff237c0688d08190
Xinyuan(Christine) Cui <1548232935@qq.com>
2026-04-01 21:34:52 -0400

    fix the error in query 2

--- query02.sql
+++ query02.sql
@@ -32,5 +32,5 @@ select
     pop.estimated_pop_800m
 from septa_bus_stop_surrounding_population as pop
 inner join septa.bus_stops as stops using (stop_id)
-order by pop.estimated_pop_800m asc
+order by pop.estimated_pop_800m asc, stops.stop_id asc
 limit 8
